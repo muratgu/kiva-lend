0f8bfd5a4dbf48973db3c24a1420a37c6ed249fb
muratgu <mgungora@gmail.com>
2021-06-03 16:22:49 -0700

    add chalk

--- kiva.js
+++ kiva.js
@@ -1,34 +1,42 @@
 const { chromium } = require('playwright');
 const dotenv = require('dotenv');
+const figlet = require('figlet')
+const clear = require('clear')
+const clui = require('clui')
+const chalk = require('chalk')
+const log = console.log;
 
 (async () => {
+
+  clear()
+  log(chalk.green(figlet.textSync('KivaBot', { horizontalLayout: 'full' }) ));
 
   dotenv.config()
 
   const for_real = process.argv.find(x => x == '--for-real')
-  console.log(for_real?'For real':'Dry run (--for-real to turn off)')
+  log(chalk.blue(for_real?'For real':'Dry run (--for-real to turn off)'));
 
   const headfull = process.argv.find(x => x == '--headfull')
-  console.log(headfull?'Headfull':'Headless (--headfull to turn off)')
+  log(chalk.blue(headfull?'Headfull':'Headless (--headfull to turn off)'));
 
   if (process.env.EMAIL && process.env.PASSWORD) {
     //ok
   } else {
-    console.log('EMAIL and PASSWORD required')
+    log(chalk.red('EMAIL and PASSWORD required'));
     process.exit(-1)
   }
 
-  console.log('Launch browser')
+  log(chalk.green('Launching browser'))
   const browser = await chromium.launch({
     headless: !headfull
   });
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  console.log('Go to https://www.kiva.org/')
+  log(chalk.green('Navigating to https://www.kiva.org/'))
   await page.goto('https://www.kiva.org/');
   
-  console.log('Sign in')
+  log(chalk.green('Signing in'))
   await Promise.all([
     page.waitForNavigation(),
     page.click('text=Sign in')
@@ -43,10 +51,10 @@ const dotenv = require('dotenv');
   await page.waitForTimeout(500);
 
   const amount = await (await page.textContent('a.header-button.my-kiva .amount')).replace('$','')
-  console.log('Amount left = ' + amount)
+  log(chalk.green('Amount left = ' + amount));
 
   if (Number(amount) >= 25) {
-    console.log('Search')
+    log(chalk.green('Searching'))
     const filter = 'country=kg,tj&sector=1&sortBy=amountLeft'
     await page.goto('https://www.kiva.org/lend/?' + filter)
     await page.waitForTimeout(1500);
@@ -55,45 +63,45 @@ const dotenv = require('dotenv');
     const borrower_country = await (await page.textContent('.loan-card-2-country')).trim()
     const borrower_use = await (await page.textContent('.loan-card-2-use')).trim()
 
-    console.log(`${borrower_name} from ${borrower_country}. ${borrower_use}`)
+    log(chalk.green(`${borrower_name} from ${borrower_country}. ${borrower_use}`));
 
-    console.log('Lend $25')
-    await page.click('text=Lend $25');
+    log(chalk.green('Lending $25'))
+    await page.click('text=Lend $25')
 
-    console.log('Checkout now')
+    log(chalk.green('Checking out'))
     // Click text=Checkout now
     await Promise.all([
       page.waitForNavigation(/*{ url: 'https://www.kiva.org/checkout' }*/),
       page.click('text=Checkout now')
     ]);
 
-    console.log('Remove the donation to Kiva')
+    log(chalk.green('Removing the donation to Kiva'))
     await page.click('text=$3.75 Edit Donation');
     // Click text=No donation to Kiva
     await page.click('text=No donation to Kiva');
     await page.waitForTimeout(1500);
 
     const total_value = await (await page.textContent('.total-value')).replace('(','').replace(')','')
-    console.log(`Total value ${total_value}`)
+    log(chalk.green(`Total value ${total_value}`));
     if (total_value != '$25.00') {    
-      console.log('Total value expected to be $25.00')
+      log(chalk.red('Total value expected to be $25.00'));
       process.exit(-1)
     }
 
-    console.log('Complete order')
+    log(chalk.green('Completing the order'))
     if (for_real) {
-      await page.click('button#kiva-credit-payment-button');
-      await page.waitForTimeout(1500);
+      await page.click('button#kiva-credit-payment-button')
+      await page.waitForTimeout(1500)
     } else {
-      console.log('(DRYRUN) click button#kiva-credit-payment-button');
+      log(chalk.red('(DRYRUN) click button#kiva-credit-payment-button'));
     }
   } else {
-    console.log('Not enough funds to lend')
+    log(chalk.red('Not enough funds to lend'));
   }
 
-  console.log('Sign out')
+  log(chalk.green('Signing out'))
   await page.goto('https://www.kiva.org/logout')
-
+  
   await context.close()
   await browser.close()
 })();
